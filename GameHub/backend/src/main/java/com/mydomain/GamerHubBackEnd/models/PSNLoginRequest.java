2eb694a2c472d64aa1573ee14ab6b887a73bd130
ahmad bishara <abishara@mail.endicott.edu>
2024-12-17 16:56:37 -0500

    initial commit

---
+++ GameHub/backend/src/main/java/com/mydomain/GamerHubBackEnd/models/PSNLoginRequest.java
@@ -0,0 +1,39 @@
+package com.mydomain.GamerHubBackEnd.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PSNLoginRequest {
+    @JsonProperty("AuthCode")
+    private String authCode;
+
+    @JsonProperty("RedirectUri")
+    private String redirectUri;
+
+    @JsonProperty("CreateAccount")
+    private boolean createAccount;
+
+    // Getters and Setters
+    public String getAuthCode() {
+        return authCode;
+    }
+
+    public void setAuthCode(String authCode) {
+        this.authCode = authCode;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public void setRedirectUri(String redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+
+    public boolean isCreateAccount() {
+        return createAccount;
+    }
+
+    public void setCreateAccount(boolean createAccount) {
+        this.createAccount = createAccount;
+    }
+}
