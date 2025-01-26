9bec07cf3f214960d469f32f906da331e5e46ec7
ahmad bishara <abishara@mail.endicott.edu>
2025-01-26 18:33:47 -0500

    working on search user

--- GameHub/frontend/src/service/RecentGamesXbox.js
+++ GameHub/frontend/src/service/RecentGamesXbox.js
@@ -6,12 +6,18 @@ const apiUrl = 'http://localhost:8080';
 export const getRecentGames = async () => {
     const uhs = localStorage.getItem("uhs");
     const XSTS_token = localStorage.getItem("XSTS_token");
+    const jwtToken = localStorage.getItem("jwtToken");
+    if(jwtToken === null){
+        console.error("User is not authenticated");
+        return {success: false, message: "User is not authenticated"};
+    }
     try{
         const response = await fetch(`${apiUrl}/api/xbox/recent-games`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `XBL3.0 x=${uhs};${XSTS_token}`,
+                'Auth': 'Bearer ' + jwtToken,
             },
         });
         if(!response.ok){
